6738f101a0a07605f1854f65b6a8977697614460
Grey Christoforo <grey@christoforo.net>
2016-01-29 16:00:47 +0000

    fixup gpg keyserver stuff

--- add-aur.sh
+++ add-aur.sh
@@ -8,19 +8,29 @@ AUR_USER=$1
 # create the user
 useradd -m $AUR_USER
 
+# setup gpg for this new user
+su $AUR_USER -c 'gpg --refresh-keys'
+su $AUR_USER -c 'sed -i "s,#keyserver-options auto-key-retrieve,keyserver-options auto-key-retrieve,g" ~/.gnupg/gpg.conf'
+su $AUR_USER -c 'echo "keyserver hkp://pgp.mit.edu" >> ~/.gnupg/gpg.conf'
+
 # install devel packages
 pacman -S --needed --noconfirm base-devel
 
 # give the aur user passwordless sudo powers
 echo "$AUR_USER      ALL = NOPASSWD: ALL" >> /etc/sudoers
 
+# use all possible cores for subsequent package builds
+sed -i 's,#MAKEFLAGS="-j2",MAKEFLAGS="-j$(nproc)",g' /etc/makepkg.conf
+
+# don't compress the packages built here
+sed -i "s,PKGEXT='.pkg.tar.xz',PKGEXT='.pkg.tar',g" /etc/makepkg.conf
+
 # install pacaur
-su $AUR_USER -c 'gpg --keyserver pgp.mit.edu --recv-keys F56C0C53'
 su $AUR_USER -c 'cd; bash <(curl aur.sh) -si --noconfirm cower pacaur'
 su $AUR_USER -c 'cd; rm -rf cower pacaur'
 
 # do a pacaur system update
 su $AUR_USER -c 'pacaur -Syyua --noedit --noconfirm'
 
-echo "This container is now set up for AUR access by using the following command:"
+echo "Packages from the AUR can now be installed like this:"
 echo "su $AUR_USER -c 'pacaur -S --noedit --noconfirm PACKAGE'"
