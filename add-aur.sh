0a656de0666d57778299d1112b53d3b79e68e8f6
Greyson Christoforo <grey@christoforo.net>
2021-02-03 10:18:14 +0000

    some syntax updates

--- add-aur.sh
+++ add-aur.sh
@@ -13,10 +13,10 @@ HELPER="${2:-yay}"
 pacman -Syyu git sudo --needed --noprogressbar --noconfirm
 
 # create the user
-useradd ${AUR_USER} --system --shell /usr/bin/nologin --create-home --home-dir "/var/${AUR_USER}"
+useradd "${AUR_USER}" --system --shell /usr/bin/nologin --create-home --home-dir "/var/${AUR_USER}"
 
 # lock out the ${AUR_USER}'s password
-passwd --lock ${AUR_USER}
+passwd --lock "${AUR_USER}"
 
 # give the aur user passwordless sudo powers for pacman
 echo "${AUR_USER} ALL=(ALL) NOPASSWD: /usr/bin/pacman" > "/etc/sudoers.d/allow_${AUR_USER}_to_pacman"
@@ -31,26 +31,26 @@ sed 's,^#MAKEFLAGS=.*,MAKEFLAGS="-j$(nproc)",g' -i /etc/makepkg.conf
 sed "s,^PKGEXT=.*,PKGEXT='.pkg.tar',g" -i /etc/makepkg.conf
 
 # get helper pkgbuild
-sudo -u $AUR_USER -D~ bash -c "git clone https://aur.archlinux.org/${HELPER}.git"
+sudo -u "$AUR_USER" -D~ bash -c "git clone https://aur.archlinux.org/${HELPER}.git"
 
 # get helper deps
-makedeps=( $(source "/var/${AUR_USER}/${HELPER}/PKGBUILD" && printf '%s ' "${makedepends[@]}") )
-deps=( $(source "/var/${AUR_USER}/${HELPER}/PKGBUILD" && printf '%s ' "${depends[@]}") )
+HELPER_MAKEDEPS=( $(source "/var/${AUR_USER}/${HELPER}/PKGBUILD" && printf '%s ' "${makedepends[@]}") )
+HELPER_DEPS=( $(source "/var/${AUR_USER}/${HELPER}/PKGBUILD" && printf '%s ' "${depends[@]}") )
 
 # install deps (they must all be non-aur)
-pacman -Syyu $deps $makedeps --needed --noprogressbar --noconfirm --asdeps
+pacman -Syyu ${HELPER_DEPS} ${HELPER_MAKEDEPS} --needed --noprogressbar --noconfirm --asdeps
 
 # make helper
-sudo -u $AUR_USER -D~//${HELPER} bash -c "makepkg"
+sudo -u "${AUR_USER}" -D~//${HELPER} bash -c "makepkg"
 
 # install helper
-pushd /var/"${AUR_USER}"/${HELPER}
+pushd /var/"${AUR_USER}/${HELPER}"
 pacman -U *.pkg.tar --noprogressbar --noconfirm
 popd
-sudo -u $AUR_USER -D~ bash -c "rm -rf ${HELPER}"
+sudo -u "${AUR_USER}" -D~ bash -c "rm -rf ${HELPER}"
 
 # this must be a bug in yay's PKGBUILD...
-sudo -u $AUR_USER -D~ bash -c "rm -rf .cache/go-build"
+sudo -u "${AUR_USER}" -D~ bash -c "rm -rf .cache/go-build"
 # go clean -cache  # alternative cache clean
 
 # chuck deps
@@ -59,15 +59,15 @@ pacman -Qtdq | pacman -Rns - --noconfirm
 # setup storage for AUR packages built in the future
 sed -i '/PKGDEST=/c\PKGDEST=/var/cache/makepkg/pkg' -i /etc/makepkg.conf
 mkdir -p /var/cache/makepkg
-install -o $AUR_USER -d /var/cache/makepkg/pkg
+install -o "${AUR_USER}" -d /var/cache/makepkg/pkg
 
-if [ "$HELPER" == "yay" ] || [ "$HELPER" == "paru" ]
+if [ "${HELPER}" == "yay" ] || [ "${HELPER}" == "paru" ]
 then
   # do a helper system update just to ensure yay is working
-  sudo -u $AUR_USER -D~ bash -c "${HELPER} -Syyu --noprogressbar --noconfirm --needed"
+  sudo -u "${AUR_USER}" -D~ bash -c "${HELPER} -Syyu --noprogressbar --noconfirm --needed"
 
   # cache clean
-  sudo -u $AUR_USER -D~ bash -c "yes | ${HELPER} -Scc"
+  sudo -u "${AUR_USER}" -D~ bash -c "yes | ${HELPER} -Scc"
   
   echo "Packages from the AUR can now be installed like this:"
   echo "sudo -u ${AUR_USER} -D~ bash -c '${HELPER} -Suy --needed --removemake --noprogressbar --noconfirm PACKAGE'"
